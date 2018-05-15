f44b47fb2297a631ad825e6213013e0e5bbb90f0
Ricardo Aoun <contato@aoun.com.br>
2018-05-15 18:47:30 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/aoun/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.aoun.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aoun.cursomc.domain.Categoria;
+import com.aoun.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Categoria obj = repo.findOne(id);
+		return obj;
+		
+	}
+
+}
