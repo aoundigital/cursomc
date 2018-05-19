3b92745eb06bd78b7ad96247c4e03bcde3527d7a
Ricardo Aoun <contato@aoun.com.br>
2018-05-19 17:18:34 -0300

    Esqueci a senha e Refresh Token

--- src/main/java/com/aoun/cursomc/services/EmailService.java
+++ src/main/java/com/aoun/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.aoun.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.aoun.cursomc.domain.Cliente;
 import com.aoun.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
